663822b40e6b073cb1d010a936cf8aaeb987bac3
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2014-12-30 01:37:38 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -16,5 +16,18 @@ setup(
     author_email='ich@danielneuhaeuser.de',
     url='https://github.com/DasIch/oore',
 
-    py_modules=['oore']
+    py_modules=['oore'],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
 )
